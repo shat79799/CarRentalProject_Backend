5caeb1cd9a76b3f24fc684cd1ce36c48c7b91dff
shat79799 <shat79799@gmail.com>
2025-11-21 16:46:38 +0800

    add annotation @CrossOrigin

--- sbcar/src/main/java/com/demo/controller/CarController.java
+++ sbcar/src/main/java/com/demo/controller/CarController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
